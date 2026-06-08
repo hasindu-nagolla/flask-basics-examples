fc1c63b7b1347a510c86653c7fba96082459ba4e
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2026-06-08 19:37:29 +0530

    vite --open

--- 08-cookies/app.py
+++ 08-cookies/app.py
@@ -6,18 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('setcookie.html')
 
-@app.route('/setcookie', methods = ['POST', 'GET'])
+@app.route('/setcookie', methods=['POST', 'GET'])
 def setcookie():
     if request.method == 'POST':
-        user = request.form[nm]
+        user = request.form['nm']
         resp = make_response(render_template('readcookie.html'))
         resp.set_cookie('userID', user)
         return resp
-    
+
+
 @app.route('/getcookie')
 def getcookie():
     name = request.cookies.get('userID')
-    return '<h1>welcome' +name+'<h1>'
+    return '<h1>welcome ' + name + '</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
